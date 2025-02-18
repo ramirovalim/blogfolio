fb5b30ae0e2edda93404ba6082ca86a70bce718b
ramiro <ramirotuk@gmail.com>
2025-02-18 03:00:12 -0300

    first commit

---
+++ routes/index.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div class="px-4 py-8 mx-auto">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <img
+          class="my-6"
+          src="/logo.svg"
+          width="128"
+          height="128"
+          alt="the Fresh logo: a sliced lemon dripping with juice"
+        />
+        <h1 class="text-4xl text-white font-bold">Ramiro Valim</h1>
+        <p class="my-4 text-white">
+          Welcome to my <span>blogfolio</span> website :)
+        </p>
+      </div>
+    </div>
+  );
+}
